7eeb83b6126e2660610a3f3324a08a434080c75e
kampkelly <kampkellykeys@gmail.com>
2018-03-06 04:46:24 +0100

    add mlab database

--- app/models/Chat.js
+++ app/models/Chat.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/kampbox');
+//mongoose.connect('mongodb://localhost:27017/kampbox');
+mongoose.connect('mongodb://kamp:sweetie@ds253468.mlab.com:53468/kampbox_mychat');
 var Schema = mongoose.Schema;
 
 // define the schema for our user model
